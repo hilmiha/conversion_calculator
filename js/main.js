3d6c216cf5ab19100949a65a6186c00bdc454fe7
Hilmi Hidayat Arfisko <hilmihidayatar@gmail.com>
2022-08-05 19:58:42 +0700

    Button pilihan-pilihan unit sekarang dibuat menggunakan createElement JS

--- js/main.js
+++ js/main.js
@@ -13,6 +13,52 @@ window.addEventListener("load", function() {
     
 });
 
+function createbutton(jenis, units){
+    let btnlistfrom_container = document.getElementById("btn_list_from");
+    let btnlistto_container = document.getElementById("btn_list_to");
+    
+    while (btnlistfrom_container.firstChild) {
+        btnlistfrom_container.removeChild(btnlistfrom_container.firstChild);
+        btnlistto_container.removeChild(btnlistto_container.firstChild)
+    }
+    units.forEach(unit => {
+
+        // create button unit input
+        let buttonfrom_container = document.createElement('div');
+        buttonfrom_container.className = "col-6 mb-3";
+        buttonfrom_container.tagName = jenis+"_btn";
+
+        let button_from = document.createElement('button');
+        button_from.type = "button";
+        button_from.className = "btn btn-outline-secondary w-100";
+        button_from.id = "btn_from_"+unit[0];
+        button_from.onclick = function () {
+            setUnitForm(unit[0],button_from.id);
+        };
+        button_from.textContent = unit[1];
+
+        buttonfrom_container.appendChild(button_from);
+        btnlistfrom_container.appendChild(buttonfrom_container);
+
+        // create button unit output
+        let buttonto_container = document.createElement('div');
+        buttonto_container.className = "col-6 mb-3";
+        buttonto_container.tagName = jenis+"_btn";
+
+        let button_to = document.createElement('button');
+        button_to.type = "button";
+        button_to.className = "btn btn-outline-secondary w-100";
+        button_to.id = "btn_to_"+unit[0];
+        button_to.onclick = function () {
+            setUnitTo(unit[0],button_to.id);
+        };
+        button_to.textContent = unit[1];
+
+        buttonto_container.appendChild(button_to);
+        btnlistto_container.appendChild(buttonto_container);
+    });
+}
+
 let btn_menu_prev
 let btn_from_prev
 let btn_to_prev
@@ -21,12 +67,28 @@ function prep_mode(jenis){
     if (jenis=="panjang"){
         input.jenis = "panjang";
 
+        let panjang_units = [
+            ["km", "Kilometer"], 
+            ["hm", "Hektometer"],
+            ["dam", "Dekameter"],
+            ["m", "Meter"],
+            ["dm", "Desimeter"],
+            ["cm", "Centimeter"],
+            ["mm", "Milimeter"],
+            ["mile", "Mile"],
+            ["yrd", "Yard"],
+            ["ft", "Foot"],
+            ["inc", "Inch"]
+        ]
+
         document.getElementById("judul").innerHTML = "Panjang";
         document.getElementById("jenis").value = "panjang"
         document.getElementById("nilai_x").value = "";
         document.getElementById("nilai_y").value = "";
         document.getElementById("unit_from").value = "km";
         document.getElementById("unit_to").value = "km";
+
+        createbutton(jenis, panjang_units);
 
         btn_menu_prev = "menu_btn_panjang";
         btn_from_prev = "btn_from_km";
@@ -39,12 +101,20 @@ function prep_mode(jenis){
     } else if (jenis=="suhu"){
         input.jenis = "suhu";
 
+        let suhu_units = [
+            ["c", "Celsius"], 
+            ["k", "Kelvin"],
+            ["f", "Fahrenheit"]
+        ]
+
         document.getElementById("judul").innerHTML = "Suhu";
         document.getElementById("jenis").value = "suhu"
         document.getElementById("nilai_x").value = "";
         document.getElementById("nilai_y").value = "";
         document.getElementById("unit_from").value = "c";
         document.getElementById("unit_to").value = "c";
+
+        createbutton(jenis, suhu_units);
 
         btn_menu_prev = "menu_btn_suhu";
         btn_from_prev = "btn_from_c";
@@ -54,22 +124,6 @@ function prep_mode(jenis){
         document.getElementById(btn_from_prev).classList.add("active");
         document.getElementById(btn_to_prev).classList.add("active");
     }
-
-    const jenis_list = ['panjang','suhu'];
-
-    jenis_list.forEach(e => {
-        if (jenis != e){
-            element = document.getElementsByName(e + "_btn");
-            element.forEach(button => {
-                button.classList.add("d-none");
-            });
-        } else {
-            element = document.getElementsByName(e + "_btn");
-            element.forEach(button => {
-                button.classList.remove("d-none");
-            });
-        }
-    });
 }
 
 function setMode(jenis){
@@ -123,8 +177,6 @@ function convert(){
                     nilaiout_holder.value = "Jenis Not Defined!"
                 }
             }
-
-            
         } else {
             nilaiin_holder.classList.add("is-invalid");
             nilaiout_holder.classList.add("is-invalid");
